4119894a5aab4470b1ec92e5ef4d380f32f536fd
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-03-04 19:38:24 +0100

    deleted comments

--- src/main/java/org/example/model/Orders.java
+++ src/main/java/org/example/model/Orders.java
@@ -1,13 +1,9 @@
 package org.example.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Orders {
     @Id
     private int id;
     private int client_id;
-//    private List<ProductsInOrder> products = new ArrayList<>();;
     public Orders() {}
 
     public Orders(int client_id) {
@@ -26,12 +22,6 @@ public class Orders {
     public void setClientId(int client_id) {
         this.client_id = client_id;
     }
-//    public List<ProductsInOrder>  getProducts() {
-//        return products;
-//    }
-//    public void addProducts(ProductsInOrder product) {
-//        products.add(product);
-//    }
     @Override
     public String toString(){
         return "Order{id="+id+", " +
